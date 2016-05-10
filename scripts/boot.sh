359d9cfe97066be2b92e3a01eb195e05d2e3003f
Pedro Dias <petermdias@gmail.com>
2016-05-10 03:44:29 +0100

    typo

--- scripts/boot.sh
+++ scripts/boot.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sleep 15
-/bin/bash /home/pi/probe/scripts/start_wlan &
+/bin/bash /home/pi/probe/scripts/start_wlan.sh &
 sleep 15
-/bin/bash /home/pi/probe/scripts/start_probe
+/bin/bash /home/pi/probe/scripts/start_probe.sh
 sleep 15
-/bin/bash /home/pi/probe/scripts/start_bluelog > /tmp/start_bluelog.log 2>&1
+/bin/bash /home/pi/probe/scripts/start_bluelog.sh > /tmp/start_bluelog.log 2>&1
